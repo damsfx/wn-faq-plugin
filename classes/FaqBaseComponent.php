2a62b1ddf2438837dbdb4551c84e9170fe608717
Damien MATHIEU (DamsFX) <damsfx@gmail.com>
2026-08-26 17:17:03 +0200

    fix(components): escape JSON-LD special characters

--- classes/FaqBaseComponent.php
+++ classes/FaqBaseComponent.php
@@ -158,7 +158,7 @@ abstract class FaqBaseComponent extends ComponentBase
                     ],
                 ];
             })->values()->toArray(),
-        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
     }
 
     /**
